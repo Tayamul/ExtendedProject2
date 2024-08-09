c106c6e27a179814715da8d5d3a7f78576a07543
Tayamul <rai_tayamul@hotmail.com>
2024-08-09 18:21:41 +0100

    implement tests for 500 error

--- test/services/GitHubServiceSpec.scala
+++ test/services/GitHubServiceSpec.scala
@@ -32,7 +32,7 @@ class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with
 
     "return a Right" when {
 
-      "retrieve the user object for a particular user" in {
+      "GitHub connector .get() returns a user object" in {
 
         (mockConnector.getUserByUserName[GitHubUser](_: String)(_: OFormat[GitHubUser], _: ExecutionContext))
           .expects(url, *, *)
@@ -48,9 +48,23 @@ class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with
 
     "return a Left" when {
 
-      "Github connector .get() return an error" in {
+      "Github connector .get() returns a 404 Not Found Error" in {
 
         val apiError: APIError = APIError.BadAPIResponse(404, "Not Found.")
+
+        (mockConnector.getUserByUserName[GitHubUser](_: String)(_: OFormat[GitHubUser], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserByUserName(Some(url), "username").value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+
+      "Github connector .get() returns a 500 Internal Server Error" in {
+
+        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.®")
 
         (mockConnector.getUserByUserName[GitHubUser](_: String)(_: OFormat[GitHubUser], _: ExecutionContext))
           .expects(url, *, *)
