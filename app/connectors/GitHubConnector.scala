2c29c9aee1df2e799f884d523e422807355395c2
Tayamul <rai_tayamul@hotmail.com>
2024-08-09 13:45:29 +0100

    inject wsClient

--- app/connectors/GitHubConnector.scala
+++ app/connectors/GitHubConnector.scala
@@ -1,9 +1,11 @@
 package connectors
 
+import cats.data.EitherT
 import com.google.inject._
 import play.api.libs.ws.WSClient
 
 @Singleton
 class GitHubConnector @Inject()(ws: WSClient) {
 
+//  def getUserByUserName[Response](url: String)
 }
