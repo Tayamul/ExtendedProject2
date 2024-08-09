d047b18d0e2dbe10b1fef0f548d8894ea1fa8031
Tayamul <rai_tayamul@hotmail.com>
2024-08-09 16:28:32 +0100

    define the getUser method in service to return either an error or a valid response to the controller

--- app/services/GitHubService.scala
+++ app/services/GitHubService.scala
@@ -12,16 +12,10 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class GitHubService @Inject()(gitHubConnector: GitHubConnector) {
 
-  def getUserByUserName(url: String, username: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, GitHubUser] = {
+  def getUserByUserName(username: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, GitHubUser] = {
     val url = s"https://api.github.com/users/$username"
-    val result = gitHubConnector.getUserByUserName[GitHubUser](url)
-//
-//    result.map { gitHubResponse =>
-//      gitHubResponse match {
-//        case git
-//      }
-//
-//    }
+    val userOrError = gitHubConnector.getUserByUserName[GitHubUser](url)
+    userOrError
   }
 
 }
