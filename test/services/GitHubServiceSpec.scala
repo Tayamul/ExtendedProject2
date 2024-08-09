f26aaf44b2beb7272a2f47cacd64786faf0d4f69
Tayamul <rai_tayamul@hotmail.com>
2024-08-09 17:16:41 +0100

    declare a user object for testing purposes

--- test/services/GitHubServiceSpec.scala
+++ test/services/GitHubServiceSpec.scala
@@ -2,6 +2,7 @@ package services
 
 import baseSpec.BaseSpec
 import connectors.GitHubConnector
+import models.GitHubUser
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
@@ -13,5 +14,16 @@ class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with
   val mockConnector: GitHubConnector = mock[GitHubConnector]
   implicit val ec: ExecutionContext = app.injector.asInstanceOf[ExecutionContext]
   val testService: GitHubService = new GitHubService(mockConnector)
+
+  val username: GitHubUser = GitHubUser(
+    "testUserName",
+    Some("London"),
+    50,
+    25,
+    "08/08/2024",
+    "www.github.com",
+    Some("testName")
+  )
+
 
 }
