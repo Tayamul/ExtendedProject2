dc21b93c196e840c20e4aa7abb76993987ca7f88
Tayamul <rai_tayamul@hotmail.com>
2024-08-09 15:12:13 +0100

    create a method to fetch user data from the GitHub API

--- app/connectors/GitHubConnector.scala
+++ app/connectors/GitHubConnector.scala
@@ -1,11 +1,30 @@
 package connectors
 
 import cats.data.EitherT
+import cats.data.EitherT.rightT
 import com.google.inject._
-import play.api.libs.ws.WSClient
+import models.{APIError, GitHubUser}
+import play.api.libs.json.{JsError, JsSuccess, OFormat}
+import play.api.libs.ws.{WSClient, WSResponse}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class GitHubConnector @Inject()(ws: WSClient) {
 
-//  def getUserByUserName[Response](url: String)
+  def getUserByUserName[GitHubUser](url: String)(implicit rds: OFormat[GitHubUser], ec: ExecutionContext): Future[Either[APIError, GitHubUser]] = {
+    val request = ws.url(url)
+    val response = request.get()
+
+    response.map { result =>
+      val json = result.json
+      json.validate[GitHubUser] match {
+        case JsSuccess(user, _) => Right(user)
+        case JsError(errors) => Left(APIError.BadAPIResponse(500, s"Error parsing JSON response. Message: ${errors}"))
+      }
+    }.recover { case _: WSResponse =>
+      Left(APIError.BadAPIResponse(500, "Could not connect to the database."))
+    }
+  }
+
 }
