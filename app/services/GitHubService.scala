0d65df083ce84908348f5b883a3e0f91b9b6c076
Tayamul <rai_tayamul@hotmail.com>
2024-08-09 15:44:41 +0100

    create a GitHubService class and inject the required dependencies

--- app/services/GitHubService.scala
+++ app/services/GitHubService.scala
@@ -1,5 +1,27 @@
 package services
 
-class GitHubService {
+import cats.data.EitherT
+import connectors.GitHubConnector
+import models.{APIError, GitHubUser}
+import play.api.libs.json.OFormat
+import play.shaded.ahc.org.asynchttpclient.Response
+
+import javax.inject._
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class GitHubService @Inject()(gitHubConnector: GitHubConnector) {
+
+  def getUserByUserName(url: String, username: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, GitHubUser] = {
+    val url = s"https://api.github.com/users/$username"
+    val result = gitHubConnector.getUserByUserName[GitHubUser](url)
+//
+//    result.map { gitHubResponse =>
+//      gitHubResponse match {
+//        case git
+//      }
+//
+//    }
+  }
 
 }
