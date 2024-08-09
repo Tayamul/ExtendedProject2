3e772dcbb5f2d51b655cf9835c64645dcc13653b
Tayamul <rai_tayamul@hotmail.com>
2024-08-09 17:27:13 +0100

    start testing the first test case

--- test/services/GitHubServiceSpec.scala
+++ test/services/GitHubServiceSpec.scala
@@ -1,13 +1,15 @@
 package services
 
 import baseSpec.BaseSpec
+import cats.data.EitherT
 import connectors.GitHubConnector
 import models.GitHubUser
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.libs.json.OFormat
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with GuiceOneServerPerSuite {
 
@@ -25,5 +27,17 @@ class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with
     Some("testName")
   )
 
+  "getUserByUserName" should {
+    val url = "testUrl"
 
+    "retrieve the user object for a particular user" in {
+
+      (mockConnector.getUserByUserName[GitHubUser](_: String)(_: OFormat[GitHubUser], _: ExecutionContext))
+        .expects(url, *, * )
+        .returning(EitherT.rightT(username))
+        .once()
+
+      whenReady()
+    }
+  }
 }
