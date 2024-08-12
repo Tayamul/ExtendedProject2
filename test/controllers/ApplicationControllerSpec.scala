dbc6e73c9d295ef571c21f420ce1a4333be68741
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-08-12 12:41:39 +0100

    added before and after each to clear database

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,7 +1,10 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import models.DataModel
 import org.scalamock.scalatest.MockFactory
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
+import services.RepositoryService
 
 class ApplicationControllerSpec extends BaseSpecWithApplication with MockFactory {
 
@@ -11,4 +14,23 @@ class ApplicationControllerSpec extends BaseSpecWithApplication with MockFactory
     gitService
   )(executionContext)
 
+
+  val userTestDataModel: DataModel = DataModel(
+    _username = "testUserName",
+    dateCreated = "2020-10-16T09:59:16Z",
+    location = "London",
+    numFollowers = 1,
+    numFollowing = 2,
+    repoUrl = "https://api.github.com/users/tbg2003/repos",
+    name = "Test User"
+  )
+
+  "Application Controller .index" should {
+    "return 200 Ok with body" in {
+
+    }
+  }
+
+  override def beforeEach(): Unit = await(repository.deleteAll())
+  override def afterEach(): Unit = await(repository.deleteAll())
 }
