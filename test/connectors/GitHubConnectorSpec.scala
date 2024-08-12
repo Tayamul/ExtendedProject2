d637deaf0939c71c00a7662f1f02f1531f747a04
Tayamul <rai_tayamul@hotmail.com>
2024-08-12 12:50:55 +0100

    implement a test to return a 404 error when an invalid username is requested

--- test/connectors/GitHubConnectorSpec.scala
+++ test/connectors/GitHubConnectorSpec.scala
@@ -1,32 +1,92 @@
+//import connectors.GitHubConnector
+//import models.{APIError, GitHubUser}
+//import org.scalamock.scalatest.MockFactory
+//import org.scalatest.concurrent.ScalaFutures
+//import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+//import org.scalatest.wordspec.AnyWordSpecLike
+//import play.api.libs.json.{Json, OFormat}
+//import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
+//
+//import scala.concurrent.{ExecutionContext, Future}
+//
+//class GitHubConnectorSpec extends AnyWordSpecLike with ScalaFutures with MockFactory {
+//
+//  implicit val ec: ExecutionContext = ExecutionContext.global
+//  val mockWsClient: WSClient = mock[WSClient]
+//  val mockRequest: WSRequest = mock[WSRequest]
+//  val mockResponse: WSResponse = mock[WSResponse]
+//  val connector = new GitHubConnector(mockWsClient)
+//
+//  val testUser: GitHubUser = GitHubUser(
+//    "testUserName",
+//    Some("London"),
+//    50,
+//    25,
+//    "08/08/2024",
+//    "www.github.com",
+//    Some("testName")
+//  )
+//
+//  implicit val githubUserFormat: OFormat[GitHubUser] = Json.format[GitHubUser]
+//
+//  "GitHubConnector" should {
+//
+//    "handle 200 response correctly" in {
+//      (mockWsClient.url(_: String)).expects("https://api.github.com/users/username").returning(mockRequest).once()
+//      (mockRequest.get _).expects().returning(Future.successful(mockResponse)).once()
+//      (mockResponse.status _).expects().returning(200).once()
+//      // Ensure the statusText and json are not called if not needed in this test
+//      (mockResponse.statusText _).expects().never()
+//      (mockResponse.json _).expects().returning(Json.toJson(testUser)).once()
+//
+//      whenReady(connector.getUserByUserName[GitHubUser]("username").value) { result =>
+//        result shouldBe Right(testUser)
+//      }
+//    }
+//
+//    "handle non-200 response correctly" in {
+//      (mockWsClient.url(_: String)).expects("https://api.github.com/users/username").returning(mockRequest).once()
+//      (mockRequest.get _).expects().returning(Future.successful(mockResponse)).once()
+//      (mockResponse.status _).expects().returning(404).once()
+//      (mockResponse.statusText _).expects().returning("Not Found").once()
+//      (mockResponse.json _).expects().never() // Ensure this is not called
+//
+//      whenReady(connector.getUserByUserName[GitHubUser]("username").value) { result =>
+//        result shouldBe Left(APIError.BadAPIResponse(404, "Not Found"))
+//      }
+//    }
+//
+//    "handle network error correctly" in {
+//      (mockWsClient.url(_: String)).expects("https://api.github.com/users/username").returning(mockRequest).once()
+//      (mockRequest.get _).expects().returning(Future.failed(new RuntimeException("Network Error"))).once()
+//
+//      whenReady(connector.getUserByUserName[GitHubUser]("username").value) { result =>
+//        result shouldBe Left(APIError.BadAPIResponse(500, "Could not connect to API."))
+//      }
+//    }
+//  }
+//}
+
+
 package connectors
 
 import baseSpec.BaseSpec
-import models.GitHubUser
+import cats.data.EitherT
+import models.{APIError, GitHubUser}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.libs.json.{Json, OFormat}
 import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
-
 import scala.concurrent.{ExecutionContext, Future}
 
-class GitHubConnectorSpec extends BaseSpec with ScalaFutures with MockFactory with GuiceOneServerPerSuite {
+class GitHubConnectorSpec extends BaseSpec with ScalaFutures with MockFactory {
 
-  val mockWSClient: WSClient = mock[WSClient]
+  val mockWsClient: WSClient = mock[WSClient]
   val mockRequest: WSRequest = mock[WSRequest]
-  val testConnector: GitHubConnector = new GitHubConnector(mockWSClient)
-  implicit val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
-
-  val testUserJSON: String =
-    """{
-      |"login": "testUserName",
-      |"location": "London",
-      |"public_repos": 50,
-      |"followers": 25,
-      |"created_at": "08/08/2024",
-      |"blog": "www.github.com",
-      |"name": "testName"
-      |}""".stripMargin
+//  val mockResponse: WSResponse = mock[WSResponse]
+  val connector: GitHubConnector = new GitHubConnector(mockWsClient)
+//  val connector: GitHubConnector = new GitHubConnector(ws)
+  implicit val ec: ExecutionContext = ExecutionContext.global
 
   val testUser: GitHubUser = GitHubUser(
     "testUserName",
@@ -38,7 +98,7 @@ class GitHubConnectorSpec extends BaseSpec with ScalaFutures with MockFactory wi
     Some("testName")
   )
 
-  implicit val gitHubUserFormat: OFormat[GitHubUser] = Json.format[GitHubUser]
+  implicit val githubUserFormat: OFormat[GitHubUser] = Json.format[GitHubUser]
 
   "getUserByUserName" should {
     val url = "https://api.github.com/users/username"
@@ -50,15 +110,32 @@ class GitHubConnectorSpec extends BaseSpec with ScalaFutures with MockFactory wi
         (mockResponse.json _).expects().returning(Json.toJson(testUser)).once()
 
         // Mock the WSRequest to return the mockResponse
-        (mockWSClient.url(_: String)).expects(url).returning(mockRequest).once()
+        (mockWsClient.url(_: String)).expects(url).returning(mockRequest).once()
         (mockRequest.get _).expects().returning(Future.successful(mockResponse)).once()
 
-        whenReady(testConnector.getUserByUserName[GitHubUser](url).value) { result =>
+        whenReady(connector.getUserByUserName[GitHubUser](url).value) { result =>
           result shouldBe Right(testUser)
         }
       }
     }
 
-  }
+    "return a Left" when {
+      "the API response status is not 200" in {
+        val mockResponse: WSResponse = mock[WSResponse]
+        val error = APIError.BadAPIResponse(404, "Not Found")
 
+        (mockResponse.status _).expects().returning(404).twice()
+        (mockResponse.statusText _).expects().returning("Not Found").once()
+
+        (mockWsClient.url(_: String)).expects(url).returning(mockRequest).once()
+        (mockRequest.get _).expects().returning(Future.successful(mockResponse)).once()
+
+
+        whenReady(connector.getUserByUserName[GitHubUser](url).value) { result =>
+          result shouldBe Left(error)
+        }
+      }
+
+    }
+  }
 }
