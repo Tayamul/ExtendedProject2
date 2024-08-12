d17a90de84de4a111de9cdf74b660795dbde2481
Tayamul <rai_tayamul@hotmail.com>
2024-08-12 11:27:33 +0100

    implement a test to return a response body for a success scenario

--- test/connectors/GitHubConnectorSpec.scala
+++ test/connectors/GitHubConnectorSpec.scala
@@ -6,13 +6,14 @@ import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.libs.json.{Json, OFormat}
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class GitHubConnectorSpec extends BaseSpec with ScalaFutures with MockFactory with GuiceOneServerPerSuite {
 
   val mockWSClient: WSClient = mock[WSClient]
+  val mockRequest: WSRequest = mock[WSRequest]
   val testConnector: GitHubConnector = new GitHubConnector(mockWSClient)
   implicit val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
 
@@ -38,5 +39,26 @@ class GitHubConnectorSpec extends BaseSpec with ScalaFutures with MockFactory wi
   )
 
   implicit val gitHubUserFormat: OFormat[GitHubUser] = Json.format[GitHubUser]
+
+  "getUserByUserName" should {
+    val url = "https://api.github.com/users/username"
+
+    "return a Right" when {
+      "the API response status is 200" in {
+        val mockResponse = mock[WSResponse]
+        (mockResponse.status _).expects().returning(200).once()
+        (mockResponse.json _).expects().returning(Json.toJson(testUser)).once()
+
+        // Mock the WSRequest to return the mockResponse
+        (mockWSClient.url(_: String)).expects(url).returning(mockRequest).once()
+        (mockRequest.get _).expects().returning(Future.successful(mockResponse)).once()
+
+        whenReady(testConnector.getUserByUserName[GitHubUser](url).value) { result =>
+          result shouldBe Right(testUser)
+        }
+      }
+    }
+
+  }
 
 }
