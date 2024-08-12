3a3bf913b78214702c6c6453c78599ad3c4623d6
Tayamul <rai_tayamul@hotmail.com>
2024-08-12 11:05:26 +0100

    declare required user variables for testing

--- test/connectors/GitHubConnectorSpec.scala
+++ test/connectors/GitHubConnectorSpec.scala
@@ -1,9 +1,11 @@
 package connectors
 
 import baseSpec.BaseSpec
+import models.GitHubUser
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.libs.json.{Json, OFormat}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.ExecutionContext
@@ -13,5 +15,28 @@ class GitHubConnectorSpec extends BaseSpec with ScalaFutures with MockFactory wi
   val mockWSClient: WSClient = mock[WSClient]
   val testConnector: GitHubConnector = new GitHubConnector(mockWSClient)
   implicit val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
+
+  val testUserJSON: String =
+    """{
+      |"login": "testUserName",
+      |"location": "London",
+      |"public_repos": 50,
+      |"followers": 25,
+      |"created_at": "08/08/2024",
+      |"blog": "www.github.com",
+      |"name": "testName"
+      |}""".stripMargin
+
+  val testUser: GitHubUser = GitHubUser(
+    "testUserName",
+    Some("London"),
+    50,
+    25,
+    "08/08/2024",
+    "www.github.com",
+    Some("testName")
+  )
+
+  implicit val gitHubUserFormat: OFormat[GitHubUser] = Json.format[GitHubUser]
 
 }
