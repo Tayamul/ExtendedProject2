57d13fa7060700c3575bae811c36f181f7df6e7f
Tayamul <rai_tayamul@hotmail.com>
2024-08-12 16:33:08 +0100

    create a method to convert data types

--- app/services/GitHubService.scala
+++ app/services/GitHubService.scala
@@ -2,11 +2,12 @@ package services
 
 import cats.data.EitherT
 import connectors.GitHubConnector
-import models.{APIError, GitHubUser}
+import models.{APIError, DataModel, GitHubUser}
 import play.api.libs.json.OFormat
 import play.shaded.ahc.org.asynchttpclient.Response
 
 import javax.inject._
+import scala.concurrent.impl.Promise
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
@@ -16,6 +17,18 @@ class GitHubService @Inject()(gitHubConnector: GitHubConnector) {
     val url = urlOverride.getOrElse(s"https://api.github.com/users/$username")
     val userOrError = gitHubConnector.getUserByUserName[GitHubUser](url)
     userOrError
+  }
+
+  def convertDataType(user: GitHubUser): DataModel = {
+    DataModel(
+      _id = user.login,
+      dateCreated = user.created_at,
+      location = user.location.getOrElse(""),
+      numFollowers = user.followers,
+      numFollowing = user.following,
+      repoUrl = user.repos_url,
+      name = user.name.getOrElse("")
+    )
   }
 
 }
