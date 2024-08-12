9dae0e552c1000443b12ad3492867fa111705c89
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-08-12 13:04:42 +0100

    implement .read test

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,14 +1,17 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
-import models.DataModel
+import models.{APIError, DataModel}
 import org.scalamock.scalatest.MockFactory
+import play.api.http.Status
 import play.api.http.Status._
 import play.api.libs.json.{JsValue, Json}
-import play.api.mvc.AnyContent
+import play.api.mvc.{AnyContent, Result}
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{await, defaultAwaitTimeout, status}
+import play.api.test.Helpers.{await, contentAsJson, defaultAwaitTimeout, status}
 import services.RepositoryService
+
+import scala.concurrent.Future
 
 class ApplicationControllerSpec extends BaseSpecWithApplication with MockFactory {
 
@@ -43,7 +46,6 @@ class ApplicationControllerSpec extends BaseSpecWithApplication with MockFactory
 
         afterEach()
       }
-
     }
 
     "return 404 Not Found" when {
@@ -55,9 +57,50 @@ class ApplicationControllerSpec extends BaseSpecWithApplication with MockFactory
 
         afterEach()
       }
-
     }
   }
+
+  "ApplicationController .create" should {
+    "return 201 Created with body" in {
+      beforeEach()
+      val request: FakeRequest[JsValue] = testRequest.buildPost("/api").withBody[JsValue](Json.toJson(userTestDataModel))
+      val createdResult: Future[Result] = TestController.create()(request)
+
+      status(createdResult) shouldBe Status.CREATED
+      contentAsJson(createdResult).as[DataModel] shouldBe userTestDataModel
+      afterEach()
+    }
+
+    "return 500 Internal Server Error" in {
+      beforeEach()
+      val apiError:APIError = APIError.BadAPIResponse(500, "An error has occurred:")
+      val request:FakeRequest[JsValue] = testRequest.buildPost("/api/user").withBody[JsValue](Json.toJson(userTestDataModel))
+      val request2:FakeRequest[JsValue] = testRequest.buildPost("/api/user").withBody[JsValue](Json.toJson(userTestDataModel))
+      val createdResult: Future[Result] = TestController.create()(request)
+
+      status(createdResult) shouldBe OK
+      val createdResult2: Future[Result] = TestController.create()(request2)
+
+      status(createdResult2) shouldBe apiError.httpResponseStatus
+      afterEach()
+    }
+
+    "return 400 Bad Request" in {
+      beforeEach()
+      val badRequestBody:JsValue = Json.parse("""{"id": "abcd", "name": 12345}""")
+      val badRequest: FakeRequest[JsValue] = testRequest.buildPost("/api/user").withBody[JsValue](Json.toJson(badRequestBody))
+      val createdResult: Future[Result] = TestController.create()(badRequest)
+
+      status(createdResult) shouldBe Status.BAD_REQUEST
+      afterEach()
+    }
+  }
+
+  "ApplicationController .read"
+
+  "ApplicationController .update"
+
+  "ApplicationController .delete"
 
   override def beforeEach(): Unit = await(repository.deleteAll())
   override def afterEach(): Unit = await(repository.deleteAll())
