f297dedbf57ca8889e565a67359b6c634fbd170b
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-08-12 10:37:59 +0100

    implemented Index & CR methods

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,10 +1,10 @@
 package controllers
 
 import cats.data.EitherT
-import models.DataModel
+import models.APIError.BadAPIResponse
+import models.{APIError, DataModel}
 import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
-import repositories.DataRepoMethods
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request, Result}
 import services.{GitHubService, RepositoryService}
 
 import javax.inject.{Inject, Singleton}
@@ -13,21 +13,58 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class ApplicationController @Inject()(
                                        val controllerComponents: ControllerComponents,
-                                       val repoService: DataRepoMethods,
+                                       val repoService: RepositoryService,
                                        val gitHubService: GitHubService
                                      )(implicit val ec: ExecutionContext) extends BaseController {
 
+/** ---- REPO SERVICE CRUD OPERATIONS ---- */
+  private def resultError(error:APIError):Result = {
+    error match {
+      case BadAPIResponse(upstreamStatus, upstreamMessage) => Status(upstreamStatus)(Json.toJson(upstreamMessage))
+      case _ => Status(error.httpResponseStatus)(Json.toJson(error.reason))
+    }
+  }
 
-  def index(): Action[AnyContent] = ???
+  def index(): Action[AnyContent] = Action.async { implicit request =>
+    repoService.index().map{
+      case Left(error) => Status(error.upstreamStatus)(Json.toJson(error.upstreamMessage))
+      // TODO implement a no items in database view?
+      case Right(item: Seq[DataModel]) => Ok {Json.toJson(item)}
+    }
+  }
+
+  def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsError(_) => Future(BadRequest)
+      // _ = JsPath
+      case JsSuccess(dataModel, _) =>
+        repoService.create(dataModel).map{
+          case Right(createdDataModel) => Created(Json.toJson(createdDataModel))
+          case Left(error) => resultError(error)
+        }
+    }
+  }
+
+  def read(username: String): Action[AnyContent] = Action.async{implicit request: Request[AnyContent] =>
+    repoService.read(username).map {
+      case Left(error) => Status(error.upstreamStatus)(Json.toJson(error.upstreamMessage))
+      case Right(item: DataModel) => Ok(Json.toJson(item))
+    }
+  }
 
 
-  def create(): Action[JsValue] = ???
+  def update(username: String): Action[JsValue] = Action.async(parse.json){implicit request =>
+    request.body.validate[DataModel] match {
+      case JsError(errors) => Future(BadRequest)
+      case JsSuccess(dataModel, _) =>
+        repoService.update(username, dataModel).map {
+          case Left(error) => resultError(error)
+          case Right(value) => ???
+        }
 
 
-  def read(id: String): Action[AnyContent] = ???
-
-
-  def update():Action[AnyContent] = ???
+    }
+  }
 
 
   def delete():Action[AnyContent] = ???
@@ -35,7 +72,7 @@ class ApplicationController @Inject()(
 
   def getGitHubUser(username: String):Action[AnyContent] = Action.async { request =>
     gitHubService.getUserByUserName(username).value.map {
-      case Left(error) => Status(error.httpResponseStatus)(error.reason)
+      case Left(error) => resultError(error)
       case Right(user) => Ok{Json.toJson(user)}
     }
   }
