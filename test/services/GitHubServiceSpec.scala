0bce36f8fa55a9453f9aa9df06c12cef15c98cf7
Muhammad Mulla <muhammad.mulla@mercator.group>
2024-08-13 16:40:19 +0100

    GHService tests completed

--- test/services/GitHubServiceSpec.scala
+++ test/services/GitHubServiceSpec.scala
@@ -3,7 +3,7 @@ package services
 import baseSpec.BaseSpec
 import cats.data.EitherT
 import connectors.GitHubConnector
-import models.{APIError, GitHubUser}
+import models.{APIError, GitHubUser, RepoContentItem, RepoFileItem, Repository}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
@@ -17,7 +17,13 @@ class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with
   implicit val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
   val testService: GitHubService = new GitHubService(mockConnector)
 
-  val username: GitHubUser = GitHubUser(
+  val username: String = "testUserName"
+  val repoName: String = "testRepoName"
+  val filePath: String = "README.md"
+  val dirPath: String = "src/main"
+
+  // Define sample data
+  val gitHubUser: GitHubUser = GitHubUser(
     "testUserName",
     Some("London"),
     50,
@@ -27,29 +33,39 @@ class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with
     Some("testName")
   )
 
+  val repos: Seq[Repository] = Seq(
+    Repository("repo1", `private` = false, "https://github.com/user/repo1", Some("A test repo")),
+    Repository("repo2", `private` = true, "https://github.com/user/repo2", Some("Another test repo"))
+  )
+
+  val repoContent: Seq[RepoContentItem] = Seq(
+    RepoContentItem("README.md", "https://github.com/user/repo1/README.md", "file"),
+    RepoContentItem("src", "https://github.com/user/repo1/src", "dir")
+  )
+
+  val fileContent: RepoFileItem = RepoFileItem("README.md", "file", "base64content", "base64")
+
+  // Test for getUserByUserName
   "getUserByUserName" should {
-    val url = "username"
+    val url = s"https://api.github.com/users/$username"
 
     "return a Right" when {
 
       "GitHub connector .get() returns a user object" in {
-
         (mockConnector.get[GitHubUser](_: String)(_: OFormat[GitHubUser], _: ExecutionContext))
           .expects(url, *, *)
-          .returning(EitherT.rightT(username))
+          .returning(EitherT.rightT(gitHubUser))
           .once()
 
-        whenReady(testService.getUserByUserName(Some(url), "username").value) { result =>
-          result shouldBe Right(username)
+        whenReady(testService.getUserByUserName(Some(url), username).value) { result =>
+          result shouldBe Right(gitHubUser)
         }
       }
-
     }
 
     "return a Left" when {
 
-      "Github connector .get() returns a 404 Not Found Error" in {
-
+      "GitHub connector .get() returns a 404 Not Found Error" in {
         val apiError: APIError = APIError.BadAPIResponse(404, "Not Found.")
 
         (mockConnector.get[GitHubUser](_: String)(_: OFormat[GitHubUser], _: ExecutionContext))
@@ -57,27 +73,254 @@ class GitHubServiceSpec extends BaseSpec with ScalaFutures with MockFactory with
           .returning(EitherT.leftT(apiError))
           .once()
 
-        whenReady(testService.getUserByUserName(Some(url), "username").value) { result =>
+        whenReady(testService.getUserByUserName(Some(url), username).value) { result =>
           result shouldBe Left(apiError)
         }
       }
 
-      "Github connector .get() returns a 500 Internal Server Error" in {
-
-        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.®")
+      "GitHub connector .get() returns a 500 Internal Server Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.")
 
         (mockConnector.get[GitHubUser](_: String)(_: OFormat[GitHubUser], _: ExecutionContext))
           .expects(url, *, *)
           .returning(EitherT.leftT(apiError))
           .once()
 
-        whenReady(testService.getUserByUserName(Some(url), "username").value) { result =>
+        whenReady(testService.getUserByUserName(Some(url), username).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+    }
+  }
+
+  // Test for getUserRepos
+  "getUserRepos" should {
+    val url = s"https://api.github.com/users/$username/repos"
+
+    "return a Right" when {
+      "GitHub connector .get() returns a sequence of repositories" in {
+        (mockConnector.get[Seq[Repository]](_: String)(_: OFormat[Seq[Repository]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.rightT(repos))
+          .once()
+
+        whenReady(testService.getUserRepos(Some(url), username).value) { result =>
+          result shouldBe Right(repos)
+        }
+      }
+    }
+
+    "return a Left" when {
+      "GitHub connector .get() returns a 404 Not Found Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(404, "Not Found.")
+
+        (mockConnector.get[Seq[Repository]](_: String)(_: OFormat[Seq[Repository]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepos(Some(url), username).value) { result =>
           result shouldBe Left(apiError)
         }
       }
 
+      "GitHub connector .get() returns a 500 Internal Server Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.")
+
+        (mockConnector.get[Seq[Repository]](_: String)(_: OFormat[Seq[Repository]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepos(Some(url), username).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+    }
+  }
+
+  // Test for getUserRepoByRepoName
+  "getUserRepoByRepoName" should {
+    val url = s"https://api.github.com/repos/$username/$repoName"
+    val repo = repos.head // Access the first repository from the defined repos sequence
+
+    "return a Right" when {
+      "GitHub connector .get() returns a repository object" in {
+        (mockConnector.get[Repository](_: String)(_: OFormat[Repository], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.rightT(repo))
+          .once()
+
+        whenReady(testService.getUserRepoByRepoName(Some(url), username, repoName).value) { result =>
+          result shouldBe Right(repo)
+        }
+      }
     }
 
+    "return a Left" when {
+      "GitHub connector .get() returns a 404 Not Found Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(404, "Not Found.")
 
+        (mockConnector.get[Repository](_: String)(_: OFormat[Repository], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoByRepoName(Some(url), username, repoName).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+
+      "GitHub connector .get() returns a 500 Internal Server Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.")
+
+        (mockConnector.get[Repository](_: String)(_: OFormat[Repository], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoByRepoName(Some(url), username, repoName).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+    }
+  }
+
+  // Test for getUserRepoContent
+  "getUserRepoContent" should {
+    val url = s"https://api.github.com/repos/$username/$repoName/contents/$filePath"
+
+    "return a Right" when {
+      "GitHub connector .get() returns the repository content" in {
+        (mockConnector.get[Seq[RepoContentItem]](_: String)(_: OFormat[Seq[RepoContentItem]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.rightT(repoContent))
+          .once()
+
+        whenReady(testService.getUserRepoContent(Some(url), username, repoName).value) { result =>
+          result shouldBe Right(repoContent)
+        }
+      }
+    }
+
+    "return a Left" when {
+      "GitHub connector .get() returns a 404 Not Found Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(404, "Not Found.")
+
+        (mockConnector.get[Seq[RepoContentItem]](_: String)(_: OFormat[Seq[RepoContentItem]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoContent(Some(url), username, repoName).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+
+      "GitHub connector .get() returns a 500 Internal Server Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.")
+
+        (mockConnector.get[Seq[RepoContentItem]](_: String)(_: OFormat[Seq[RepoContentItem]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoContent(Some(url), username, repoName).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+    }
+  }
+
+  // Test for getUserRepoDirContent
+  "getUserRepoDirContent" should {
+    val url = s"https://api.github.com/repos/$username/$repoName/contents/$dirPath"
+
+    "return a Right" when {
+      "GitHub connector .get() returns a sequence of repo content items" in {
+        (mockConnector.get[Seq[RepoContentItem]](_: String)(_: OFormat[Seq[RepoContentItem]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.rightT(repoContent))
+          .once()
+
+        whenReady(testService.getUserRepoDirContent(None, username, repoName, dirPath).value) { result =>
+          result shouldBe Right(repoContent)
+        }
+      }
+    }
+
+    "return a Left" when {
+      "GitHub connector .get() returns a 404 Not Found Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(404, "Not Found.")
+
+        (mockConnector.get[Seq[RepoContentItem]](_: String)(_: OFormat[Seq[RepoContentItem]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoDirContent(None, username, repoName, dirPath).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+
+      "GitHub connector .get() returns a 500 Internal Server Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.")
+
+        (mockConnector.get[Seq[RepoContentItem]](_: String)(_: OFormat[Seq[RepoContentItem]], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoDirContent(None, username, repoName, dirPath).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+    }
+  }
+
+  // Test for getUserRepoFileContent
+  "getUserRepoFileContent" should {
+    val url = s"https://api.github.com/repos/$username/$repoName/contents/$filePath"
+
+    "return a Right" when {
+      "GitHub connector .get() returns a repo file item" in {
+        (mockConnector.get[RepoFileItem](_: String)(_: OFormat[RepoFileItem], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.rightT(fileContent))
+          .once()
+
+        whenReady(testService.getUserRepoFileContent(None, username, repoName, filePath).value) { result =>
+          result shouldBe Right(fileContent)
+        }
+      }
+    }
+
+    "return a Left" when {
+      "GitHub connector .get() returns a 404 Not Found Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(404, "Not Found.")
+
+        (mockConnector.get[RepoFileItem](_: String)(_: OFormat[RepoFileItem], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoFileContent(None, username, repoName, filePath).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+
+      "GitHub connector .get() returns a 500 Internal Server Error" in {
+        val apiError: APIError = APIError.BadAPIResponse(500, "Internal Server Error.")
+
+        (mockConnector.get[RepoFileItem](_: String)(_: OFormat[RepoFileItem], _: ExecutionContext))
+          .expects(url, *, *)
+          .returning(EitherT.leftT(apiError))
+          .once()
+
+        whenReady(testService.getUserRepoFileContent(None, username, repoName, filePath).value) { result =>
+          result shouldBe Left(apiError)
+        }
+      }
+    }
   }
 }
