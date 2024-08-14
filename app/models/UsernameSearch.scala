6ba6c8131e9f7a0f3d56167631f4f3f86c810173
Muhammad Mulla <muhammad.mulla@mercator.group>
2024-08-14 11:14:07 +0100

    val name amended

--- app/models/UsernameSearch.scala
+++ app/models/UsernameSearch.scala
@@ -1,16 +1,15 @@
 package models
 
 import play.api.data.Form
-import play.api.data.Forms.{mapping, nonEmptyText, text}
+import play.api.data.Forms.{mapping, nonEmptyText}
 import play.api.libs.json.{Json, OFormat}
 
-case class UsernameSearch(
-                         username: String
-                         )
-object UsernameSearch{
+case class UsernameSearch(username: String)
+
+object UsernameSearch {
   implicit val formats: OFormat[UsernameSearch] = Json.format[UsernameSearch]
 
-  val username: Form[UsernameSearch] = Form {
+  val usernameSearchForm: Form[UsernameSearch] = Form {
     mapping(
       "username" -> nonEmptyText(minLength = 3, maxLength = 39)
         .verifying("Username can only contain alphanumeric characters or hyphens", _.matches("^[a-zA-Z0-9-]+$"))
