edec8f5acaceab5e5546b714834160e4f43ff035
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-08-15 12:21:14 +0100

    new class for Personal Auth Token for future implementation

---
+++ app/models/PersonalAuthToken.scala
@@ -0,0 +1,19 @@
+package models
+
+
+import com.google.inject._
+import play.api.Configuration
+
+@Singleton
+class PersonalAuthToken @Inject()(config: Configuration) {
+  def get:String = config.underlying.getString("play.http.secret.key")
+}
+
+//import play.api.Configuration
+//import play.api.Environment
+//
+//object PersonalAuthToken {
+//  private val config = Configuration.reference
+//
+//  def get: String = config.underlying.getString("play.http.secret.key")
+//}
