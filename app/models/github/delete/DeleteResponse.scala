1c97d34833f4a31503880fa20debd4cc0da0caed
Tayamul <rai_tayamul@hotmail.com>
2024-08-16 14:30:00 +0100

    create a DelteResponse companion case class

--- app/models/github/delete/DeleteResponse.scala
+++ app/models/github/delete/DeleteResponse.scala
@@ -1,5 +1,12 @@
 package models.github.delete
 
-class DeleteResponse {
+import models.github.Commit
+import play.api.libs.json.{Json, OFormat}
 
+case class DeleteResponse(
+                        commit: Commit
+                      )
+
+object DeleteResponse {
+  implicit val formats: OFormat[DeleteResponse] = Json.format[DeleteResponse]
 }
