7a3cd9d83ddab62b00d561050325067705945818
Tayamul <rai_tayamul@hotmail.com>
2024-08-16 14:04:00 +0100

    create a DeleteFileForm companion case class

--- app/models/forms/DeleteFileForm.scala
+++ app/models/forms/DeleteFileForm.scala
@@ -1,5 +1,36 @@
 package models.forms
 
-class DeleteFileForm {
+import models.github.put.{Commiter, CreateFile}
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText, optional, text}
+import play.api.libs.json.{Json, OFormat}
 
+case class DeleteFileForm (
+                            name:String,
+                            message: String,
+                            content: String,
+                            branch: Option[String] = None,
+                            committerName: Option[String] = None,
+                            committerEmail: Option[String] = None,
+                            authorName: Option[String] = None,
+                            authorEmail: Option[String] = None,
+                          )
+
+
+object DeleteFileForm {
+
+  implicit val formats: OFormat[DeleteFileForm] = Json.format[DeleteFileForm]
+  // TODO make the branches a select option from possible branches
+  val form: Form[DeleteFileForm] = Form {
+    mapping(
+      "name" -> nonEmptyText,
+      "message" -> nonEmptyText,
+      "content" -> nonEmptyText,
+      "branch" -> optional(text),
+      "committerName" -> optional(text),
+      "committerEmail" -> optional(text),
+      "authorName" -> optional(text),
+      "authorEmail" -> optional(text),
+    )(DeleteFileForm.apply)(DeleteFileForm.unapply)
+  }
 }
